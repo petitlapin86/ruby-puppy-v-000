072b6ba883e9fee3f83f7a3eb38330ab57722e67
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-13 22:27:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
 class Dog
 
-  @@all = []
+  @@all = [] #create an empty array to store dogs 
 
 
-  attr_accessor :name
+  attr_accessor :name #reader and writer 
 
-  def initialize(name)
-    @name = name
-    @@all << self
+  def initialize(name) #initialize
+    @name = name #has a name 
+    @@all << self #pushes names to the array to store 
   end
 
-  def self.all
-    @@all.each do |dog|
+  def self.all 
+    @@all.each do |dog| #iterates over all of the dogs stored in the @@all array and puts out their name to the terminal.
       puts dog.name
     end
   end
 
-  def self.clear_all
+  def self.clear_all #is a class method that empties the @@all array 
       @@all.clear
     end 
 end
